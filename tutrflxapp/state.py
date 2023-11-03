57895af405109d8d6f5fe13784246f011011cd61
Germán Goñi <germangoni@icloud.com>
2023-11-03 12:20:34 -0300

    Changed model for dumb answering

--- tutrflxapp/state.py
+++ tutrflxapp/state.py
@@ -16,32 +16,23 @@ class State(rx.State):
     # Keep track of the chat history as a list of (question, answer) tuples.
     chat_history: list[tuple[str, str]]
 
-    def answer(self):
-        # Our chatbot has some brains now!
-        session = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": self.question}
-            ],
-            stop=None,
-            temperature=0.7,
-            stream=True,
+
+async def answer(self):
+    # Our chatbot is not very smart right now...
+    answer = "I don't know!"
+    self.chat_history.append((self.question, ""))
+
+    # Clear the question input.
+    self.question = ""
+    # Yield here to clear the frontend input before continuing.
+    yield
+
+    for i in range(len(answer)):
+        # Pause to show the streaming effect.
+        await asyncio.sleep(0.1)
+        # Add one letter at a time to the output.
+        self.chat_history[-1] = (
+            self.chat_history[-1][0],
+            answer[: i + 1],
         )
-
-        # Add to the answer as the chatbot responds.
-        answer = ""
-        self.chat_history.append((self.question, answer))
-
-        # Clear the question input.
-        self.question = ""
-        # Yield here to clear the frontend input before continuing.
         yield
-
-        for item in session:
-            if hasattr(item.choices[0].delta, "content"):
-                answer += item.choices[0].delta.content
-                self.chat_history[-1] = (
-                    self.chat_history[-1][0],
-                    answer,
-                )
-                yield
