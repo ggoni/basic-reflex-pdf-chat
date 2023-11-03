086a362fd15db92d28fe5b140debf0e239620d6b
Germán Goñi <germangoni@icloud.com>
2023-11-03 19:25:22 -0300

    Fix model

--- tutrflxapp/src/preprocessing.py
+++ tutrflxapp/src/preprocessing.py
@@ -1,33 +1,113 @@
-from PyPDF2 import PdfReader
+from multiprocessing import Pool
+from functools import partial
+import glob
 import os
 import openai
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from typing import List
+from langchain.llms import HuggingFaceHub
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
+
+load_dotenv()
+
+openai.openai_api_key = os.getenv("OPENAI_API_KEY")
 
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+class PdfVectorizer():
+    def __init__(self, pdf_docs: List[str]):
+        """
+        Initialize the PdfVectorizer class.
+
+        Args:
+
+        pdf_docs: Pdfs list
+
+        """
+        self.pdf_docs = pdf_docs
+        self.vector_store = None
+        self.embeddings = OpenAIEmbeddings()
+        self.text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len)
+        self.qa = None
+
+    def get_pdf_text(self) -> str:
+        """
+        Extract text from PDF documents.
+
+        Args:
+            pdf_docs (List[str]): List of paths to PDF documents.
+
+        Returns:
+            str: The extracted text from the PDF documents.
+        """
+        raw_text = ""
+        for pdf in self.pdf_docs:
+            pdf_reader = PdfReader(pdf)
+            for page in pdf_reader.pages:
+                raw_text += page.extract_text()
+        return raw_text
+
+    def get_text_chunks(self) -> List[str]:
+        """
+        Split the raw text into smaller chunks.
+
+        Args:
+            raw_text (str): The raw text to be split.
+
+        Returns:
+            List[str]: The list of text chunks.
+        """
+        raw_text = self.get_pdf_text()
+        text_chunks = self.text_splitter.split_text(raw_text)
+        return text_chunks
+
+    def get_vector_store(self) -> FAISS:
+        """
+        Create a vector store from the PDF documents.
+
+        Args:
+            pdf_docs (List[str]): List of paths to PDF documents.
+
+        Returns:
+            FAISS: The created vector store.
+        """
+        text_chunks = self.get_text_chunks()
+        if self.vector_store is None:
+            self.vector_store = FAISS.from_texts(
+                texts=text_chunks, embedding=self.embeddings)
+        return self.vector_store
 
 
-def get_text_chunks(raw_text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len)
+# pdf_docs = glob.glob("../../.web/public/*.pdf")
+# pdf_file_names = [os.path.basename(pdf_doc) for pdf_doc in pdf_docs]
+# print(pdf_file_names)
 
-    chunks = text_splitter.split_text(raw_text)
-    return chunks
+# PdfVectorizer = PdfVectorizer(pdf_docs)
+# print(PdfVectorizer.get_pdf_text())
 
+# print("Ahora los chunks...")
 
-def get_vector_store(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vector_store
+# print(PdfVectorizer.get_text_chunks())
+
+# vectorizador = PdfVectorizer.get_vector_store()
+# print(vectorizador.__sizeof__)
--- tutrflxapp/state.py
+++ tutrflxapp/state.py
@@ -2,22 +2,23 @@ import reflex as rx
 import os
 import openai
 from dotenv import load_dotenv
-from tutrflxapp.src.models import dumb_answer
-
+from tutrflxapp.src.models import dumb_answer, qa_machine
+from tutrflxapp.src.preprocessing import PdfVectorizer
+import glob
+from langchain.vectorstores import FAISS
+import pickle
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 class State(rx.State):
 
-    # The current question being asked.
     question: str
-    # Keep track of the chat history as a list of (question, answer) tuples.
     chat_history: list[tuple[str, str]]
     files: list[str]
 
-    async def handle_upload(
-            self, files: list[rx.UploadFile]):
+    async def handle_upload(self, files: list[rx.UploadFile]):
+        global vector_store
         """Handle the upload of file(s).
 
         Args:
@@ -33,10 +34,18 @@ class State(rx.State):
 
             # Update the files var.
             self.files.append(file.filename)
+            pdf_docs = [f".web/public/{file}" for file in self.files]
+            pdf_vectorizer = PdfVectorizer(pdf_docs)
+            vector_store = pdf_vectorizer.get_vector_store()
+            with open('vector_store.pkl', 'wb') as file:
+                pickle.dump(vector_store, file)
 
     def answer(self):
+
+        # with open('vector_store.pkl', 'rb') as file:
+        #     vector_store = pickle.load(file)
         # Our chatbot is not very smart right now...
-        answer = dumb_answer(self.question)
+        answer = qa_machine(self.question, vector_store)
         self.chat_history.append((self.question, answer))
         self.question = ""
         yield  # This clears the input field after the question is asked.
